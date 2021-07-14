186cb9c2312d3588147e32eb65ab28663d68cdaa
Lex Cao <lexcao@foxmail.com>
2021-07-15 00:59:53 +0800

    Day03: dependency inject

---
+++ di/di.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+func Greet(writer io.Writer, name string) {
+	_, err := fmt.Fprintf(writer, "Hello, %s", name)
+	if err != nil {
+		return
+	}
+}
+
+func GreeterHandler(w http.ResponseWriter, _ *http.Request) {
+	Greet(w, "world")
+}
+
+func main() {
+	log.Fatal(
+		http.ListenAndServe(":5000", http.HandlerFunc(GreeterHandler)),
+	)
+}
