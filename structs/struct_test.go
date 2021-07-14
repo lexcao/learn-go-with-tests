6d69679e725405bdab00732b6b19bbe22feb5eaf
Lex Cao <lexcao@foxmail.com>
2021-07-14 22:34:45 +0800

    Day03: structs

---
+++ structs/struct_test.go
@@ -0,0 +1,41 @@
+package structs
+
+import "testing"
+
+func TestPerimeter(t *testing.T) {
+	rect := Rectangle{10.0, 10.0}
+	got := Perimeter(rect)
+	want := 40.0
+
+	if got != want {
+		t.Errorf("got %.2f hasArea %.2f", got, want)
+	}
+}
+
+func TestArea(t *testing.T) {
+
+	checkArea := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
+
+		if got != want {
+			t.Errorf("%#v got %g hasArea %g", shape, got, want)
+		}
+	}
+
+	areaTests := []struct {
+		name    string
+		shape   Shape
+		hasArea float64
+	}{
+		{"Rectangle", Rectangle{12, 6}, 72.0},
+		{"Circle", Circle{10}, 314.1592653589793},
+		{"Triangle", Triangle{12, 6}, 36.0},
+	}
+
+	for _, tt := range areaTests {
+		t.Run(tt.name, func(t *testing.T) {
+			checkArea(t, tt.shape, tt.hasArea)
+		})
+	}
+}
