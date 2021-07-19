7c63b51e53136e11d6d9703d0270e6adef044642
Lex Cao <lexcao@foxmail.com>
2021-07-19 21:38:14 +0800

    day07: refactor pkg

--- http-server/server_integration_test.go
+++ http-server/server_integration_test.go
@@ -1,4 +1,4 @@
-package http_server
+package main
 
 import (
 	"net/http"
@@ -6,28 +6,8 @@ import (
 	"testing"
 )
 
-type InMemoryPlayerStore struct {
-	scores map[string]int
-}
-
-func (s *InMemoryPlayerStore) GetLeague() []Player {
-	var league []Player
-	for name, wins := range s.scores {
-		league = append(league, Player{name, wins})
-	}
-	return league
-}
-
-func (s *InMemoryPlayerStore) GetPlayerScore(name string) int {
-	return s.scores[name]
-}
-
-func (s *InMemoryPlayerStore) RecordWin(name string) {
-	s.scores[name]++
-}
-
 func TestRecodingWinsAndRetrievingThem(t *testing.T) {
-	store := &InMemoryPlayerStore{map[string]int{}}
+	store := NewInMemoryPlayerStore()
 	server := NewPlayerServer(store)
 	player := "Pepper"
 
